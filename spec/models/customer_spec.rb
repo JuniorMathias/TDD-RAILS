26e5c094c8049fc66b534d44cb850f024fe4bb66
junior <juniormathiias@gmail.com>
2024-03-21 19:54:10 -0300

    using factory bot and fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+
+  it 'Create a Customer' do
+    subject.name = "Junior"
+    subject.email = "junior@mathias.com"
+    subject.save
+    expect(subject.full_name).to eq("Sr. Junior")
+  end
+
+  it 'Create a Customer with fixtures' do
+   customer = customers(:jackson)
+    expect(customer.full_name).to eq("Sr. jackson")
+  end
+
+  it 'Create a Customer with a factory' do
+    customer = create(:customer)
+     expect(customer.full_name).to eq("Sr. Junior")
+  end
 end
