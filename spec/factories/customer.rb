e28088eb6c02284965a9b1a53e611f146991fe2a
junior <juniormathiias@gmail.com>
2024-03-21 20:07:22 -0300

    using a gem faker

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    name "Junior"
-    email "junior@mathias.com"
+    name Faker::Name.name
+    email Faker::Internet.email
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  fixtures :customers
+  # fixtures :customers
 
-  it 'Create a Customer' do
-    subject.name = "Junior"
-    subject.email = "junior@mathias.com"
-    subject.save
-    expect(subject.full_name).to eq("Sr. Junior")
-  end
+  # it 'Create a Customer' do
+  #   subject.name = "Junior"
+  #   subject.email = "junior@mathias.com"
+  #   subject.save
+  #   expect(subject.full_name).to eq("Sr. Junior")
+  # end
 
-  it 'Create a Customer with fixtures' do
-   customer = customers(:jackson)
-    expect(customer.full_name).to eq("Sr. jackson")
-  end
+  # it 'Create a Customer with fixtures' do
+  #  customer = customers(:jackson)
+  #   expect(customer.full_name).to eq("Sr. jackson")
+  # end
 
-  it 'Create a Customer with a factory' do
+  # it 'Create a Customer with a factory' do
+  #   customer = create(:customer)
+  #    expect(customer.full_name).to eq("Sr. Junior")
+  # end
+
+  it 'Create a Customer with a gem Faker' do
     customer = create(:customer)
-     expect(customer.full_name).to eq("Sr. Junior")
+     expect(customer.full_name).to start_with("Sr.")
   end
+  it {expect{create(:customer)}.to change {Customer.all.size}.by(1)}
 end
