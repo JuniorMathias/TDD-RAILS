18dd1d3f9f207c1e0acdc8973ca6f8a1ad9fe030
junior <juniormathiias@gmail.com>
2024-03-26 23:28:09 -0300

    used vcr with metadatas and sensitives

--- spec/httpparty/httpparty_spec.rb
+++ spec/httpparty/httpparty_spec.rb
@@ -1,5 +1,5 @@
-describe 'HTTPParty', :vcr do
-  it 'HttpParty' do
+describe 'HTTPParty' do
+  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #to_return(status: 200, body: "", headers: {'content-type': 'application/json; charset: utf-8'})
 
@@ -7,6 +7,5 @@ describe 'HTTPParty', :vcr do
       response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
       content_type = response.headers['content-type']
       expect(content_type).to match(/application\/json/)
-    end
   end
 end
