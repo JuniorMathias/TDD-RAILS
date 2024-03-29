9709dfdbf845b0a1af393991ba4cd7c9b65222cb
junior <juniormathiias@gmail.com>
2024-03-29 00:19:20 -0300

    learned how to use flash notices and content-type as json or html and should matchers for controllers

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe CustomersController, type: :controller do
   context 'as a guest' do  
 
+    it 'with valid attributes' do
+      customer_params = attributes_for(:customer)
+      puts customer_params
+    end
     it 'responds successfully' do
       get :index
       expect(response).to be_success
@@ -21,14 +25,57 @@ RSpec.describe CustomersController, type: :controller do
 
   end
 
-  context 'as logged member' do
+  describe 'as logged member' do
+    before do 
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'Content-type' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, format: :json, params: {customer: customer_params}
+      expect(response.content_type).to eq('application/json')
+    end
+
+    it 'flash notice' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, params: {customer: customer_params}
+      expect(flash[:notice]).to match(/successfully created/)
+    end
+
+    it { should route(:get, '/customers').to(action: :index)}
+
+    it 'with valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      #post is a action
+      expect {
+        post :create, params: {customer: customer_params}
+      }.to change(Customer, :count).by(1)
+    end
+
+    it 'with invalid attributes' do
+      customer_params = attributes_for(:customer, address: nil)
+      sign_in @member
+      #post is a action
+      expect {
+        post :create, params: {customer: customer_params}
+      }.not_to change(Customer, :count)
+    end
+
     it '#show (member authorized)' do
-      member = create(:member)
-      customer = create(:customer)
+      sign_in @member
 
-      sign_in member
+      get :show, params: {id: @customer.id}
+      expect(response).to have_http_status(200)
+    end
 
-      get :show, params: {id: customer.id}
+    it '#show (member authorized)' do
+      sign_in @member
+
+      get :show, params: {id: @customer.id}
       expect(response).to render_template(:show)
     end
   end
