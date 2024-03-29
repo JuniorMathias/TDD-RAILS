8ca23ec057c30c3bc25fae145bd9e6a98b07c5da
junior <juniormathiias@gmail.com>
2024-03-28 22:40:28 -0300

    controllers tested also authenticate for user logged and new model

---
+++ spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController, type: :controller do
+  it 'responds successfully' do
+    get :index
+    expect(response).to be_success
+  end
+
+  it 'responds 200 response' do
+    get :index
+    expect(response).to have_http_status(200)
+  end
+
+
+end
