875e2208085fc3cb2ca2f8c2dcf392426ecfb257
junior <juniormathiias@gmail.com>
2024-03-30 21:13:27 -0300

    created a new class where i refactored my code using page object pattern

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative '../support/new_customer_form'
 
 RSpec.feature "Customers", type: :feature, js: true do
   it 'visit index page' do
@@ -19,6 +20,18 @@ RSpec.feature "Customers", type: :feature, js: true do
     click_link('Add Message')
 
     expect(find('#my-div').find('h1')).to have_content('Yes!')
+  end
+
+  it 'create a customer- page object pattern' do
+    new_customer_form = NewCustomerForm.new
+
+    new_customer_form.login.visit_page.fill_in_with(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      address: Faker::Address.street_address
+    ).submit
+
+    expect(page).to have_content("Customer was successfully created")
   end
   
   it 'create a customer' do
